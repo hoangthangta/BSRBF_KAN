05f65800234b8c9b85c76604b584a8912dfda4a5
Hoang Thang Ta <37979473+hoangthangta@users.noreply.github.com>
2024-06-07 22:57:29 +0700

    Add files via upload

--- models/gottlieb_kan.py
+++ models/gottlieb_kan.py
@@ -68,14 +68,43 @@ class GottliebKAN(torch.nn.Module):
             x = layer(x)
         return x
 
-'''class MNISTGottliebKAN(nn.Module):
+
+class OriginalGottliebKANLayer(nn.Module):
+    def __init__(self, input_dim, output_dim, degree):
+        super(OriginalGottliebKANLayer, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.degree = degree
+        self.layernorm = nn.LayerNorm(output_dim)
+        self.alpha = nn.Parameter(torch.randn(1))
+
+        self.gottlieb_coeffs = nn.Parameter(torch.empty(input_dim, output_dim, degree + 1))
+        nn.init.normal_(self.gottlieb_coeffs, mean=0.0, std=1 / (input_dim * (degree + 1)))
+
+    def forward(self, x):
+        # Normalize x to [0, 1] using sigmoid
+        x = torch.sigmoid(x)
+
+        # Compute the Gottlieb basis functions
+        gottlieb_basis = []
+        for n in range(self.degree + 1):
+            gottlieb_basis.append(gottlieb(n, x, self.alpha))
+        gottlieb_basis = torch.stack(gottlieb_basis, dim=-1)  # shape = (batch_size, input_dim, degree + 1)
+
+        # Compute the Gottlieb interpolation
+        y = torch.einsum("bid,iod->bo", gottlieb_basis, self.gottlieb_coeffs)  # shape = (batch_size, output_dim)
+        y = y.view(-1, self.output_dim)
+        
+        return y
+        
+class OriginalGottliebKAN(nn.Module):
     def __init__(self):
-        super(MNISTGottliebKAN, self).__init__()
-        self.trigkan1 = GottliebKANLayer(784, 64, 3)
+        super(OriginalGottliebKAN, self).__init__()
+        self.trigkan1 = OriginalGottliebKANLayer(784, 64, 3)
         self.bn1 = nn.LayerNorm(64)
-        self.trigkan2 = GottliebKANLayer(64, 64, 3)
+        self.trigkan2 = OriginalGottliebKANLayer(64, 64, 3)
         self.bn2 = nn.LayerNorm(64)
-        self.trigkan3 = GottliebKANLayer(64, 10, 3)
+        self.trigkan3 = OriginalGottliebKANLayer(64, 10, 3)
 
     def forward(self, x):
         x = x.view(-1, 28*28)
@@ -84,4 +113,4 @@ class GottliebKAN(torch.nn.Module):
         x = self.trigkan2(x)
         x = self.bn2(x)
         x = self.trigkan3(x)
-        return x '''
+        return x
